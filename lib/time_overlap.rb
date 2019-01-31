34cfa64ecffbca6e0b1394003a19d635b4a39acd
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-01-31 17:04:58 +0700

    add initial implementation

--- lib/time_overlap.rb
+++ lib/time_overlap.rb
@@ -1,5 +1,76 @@
 require "time_overlap/version"
+require 'time'
+require 'active_support/core_ext/time'
 
 module TimeOverlap
-  # Your code goes here...
+
+  # TODO: add meeting time to choose overlap 1 or overlap 2
+
+  def self.count(from:, to:, time_zone:, my_time_zone:, min_overlap:)
+    current_year = Time.current.year
+    current_month = Time.current.month
+    current_day = Time.current.day
+
+    start_time = Time.new(
+      current_year,
+      current_month,
+      current_day,
+      from,
+      0,
+      0,
+      Time.zone_offset(time_zone)
+    )
+
+    end_time = Time.new(
+      current_year,
+      current_month,
+      current_day,
+      to,
+      0,
+      0,
+      Time.zone_offset(time_zone)
+    )
+
+    hours = (end_time - start_time).to_i / 60 / 60
+    offset = hours - min_overlap
+
+    start_time_in_my_time_zone = start_time.in_time_zone(my_time_zone)
+    end_time_in_my_time_zone = end_time.in_time_zone(my_time_zone)
+
+    puts "Full overlap"
+    puts [start_time.in_time_zone(Time.zone_offset(time_zone)), end_time.in_time_zone(Time.zone_offset(time_zone))].inspect
+    puts [start_time_in_my_time_zone, end_time_in_my_time_zone].inspect
+
+    x_start_time = (start_time - min_overlap * 60 * 60).in_time_zone(my_time_zone)
+    x_end_time = (end_time - offset * 60 * 60).in_time_zone(my_time_zone)
+
+    puts "Overlap 1"
+    puts [x_start_time, x_end_time].inspect
+    puts "Overlap 2"
+
+    y_start_time = (end_time - min_overlap * 60 * 60).in_time_zone(my_time_zone)
+    y_end_time = (y_start_time + 8 * 60 * 60).in_time_zone(my_time_zone)
+
+    puts [y_start_time, y_end_time].inspect
+
+    # {
+      # original: {
+        # start: start_time,
+        # end: end_time
+      # },
+      # full_overlap: {
+        # start: start_time_in_my_time_zone,
+        # end: end_time_in_my_time_zone
+      # },
+      # overlap_1: {
+        # start: x_start_time,
+        # end: x_end_time
+      # },
+      # overlap_2: {
+        # start: y_start_time,
+        # end: y_end_time
+      # }
+    # }
+    "OK"
+  end
 end
--- spec/time_overlap_spec.rb
+++ spec/time_overlap_spec.rb
@@ -3,7 +3,37 @@ RSpec.describe TimeOverlap do
     expect(TimeOverlap::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "EST, GMT-5 - Bangkok" do
+    expect(TimeOverlap.count(
+              from: 8,
+              to: 18, # 6
+              time_zone: 'EST', #"-05:00"
+              my_time_zone: 'Bangkok',
+              min_overlap: 4
+            )
+          ).to eq("OK")
+  end
+
+  it "GTM+0 - Bangkok" do
+    expect(TimeOverlap.count(
+              from: 8,
+              to: 16, # 6
+              # time_zone: '+00:00', # GMT+0
+              time_zone: '+00:00',
+              my_time_zone: 'Bangkok',
+              min_overlap: 4
+            )
+          ).to eq("OK")
+  end
+
+  it "Bangkok - Warsaw" do
+    expect(TimeOverlap.count(
+              from: 9,
+              to: 17,
+              time_zone: '+07:00',
+              my_time_zone: 'Warsaw',
+              min_overlap: 4
+            )
+          ).to eq("OK")
   end
 end
