8190d4b45b29960ee9f90bd1de4b9cb92ac0cec4
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-02-27 15:24:57 +0700

    a few modifications

--- lib/time_overlap/presenter.rb
+++ lib/time_overlap/presenter.rb
@@ -1,6 +1,15 @@
 module TimeOverlap
   class Presenter
 
+    AM = "AM ".freeze
+    PM = " PM".freeze
+    NOON = " 12:00 "
+    SIX_AM = " 6:00 "
+    SIX_PM = " 6:00 "
+
+    AVAILABLE_SLOT = "[✓]"
+    EMPTY_SLOT = "[ ]"
+
     def initialize(data, format = '%T%:z')
       @data   = data
       @format = format
@@ -10,74 +19,19 @@ module TimeOverlap
       new(*args).generate_output
     end
 
-    def show_it(s, e)
-      print"|AM| "
-      # puts (s.hour..e.hour).inspect
-
-      (0..23).each do |hour|
-        print ' |NOON| ' if hour == 12
-
-        if s.hour < e.hour
-          if (s.hour..e.hour).cover?(hour)
-            if e.hour != hour
-              print "[✓]"
-            else
-              print "[ ]"
-            end
-          else
-            print "[ ]"
-          end
-        else
-          if s.hour <= 12
-            if (e.hour..s.hour).cover?(hour)
-              if e.hour != hour
-                print "[✓]"
-              else
-                print "[ ]"
-              end
-            else
-              print "[ ]"
-            end
-          else
-            if (e.hour..s.hour).cover?(hour)
-              print "[ ]"
-            else
-              if e.hour != hour
-                print "[✓]"
-              else
-                print "[ ]"
-              end
-            end
-          end
-        end
-
-      end
-      print " |PM|"
-      puts ""
-      separator
-    end
-
     def generate_output
       puts "Original:"
       puts "#{formated_time(@data[:original][:start])} - #{formated_time(@data[:original][:end])}"
-
       separator
-
       puts "Full overlap:"
       puts "#{formated_time(@data[:full_overlap][:start])} - #{formated_time(@data[:full_overlap][:end])}"
-
-      show_it(@data[:full_overlap][:start], @data[:full_overlap][:end])
-
+      timeline(@data[:full_overlap][:start], @data[:full_overlap][:end])
       puts "Overlap 1:"
       puts "#{formated_time(@data[:overlap_1][:start])} - #{formated_time(@data[:overlap_1][:end])}"
-
-      show_it(@data[:overlap_1][:start], @data[:overlap_1][:end])
-
+      timeline(@data[:overlap_1][:start], @data[:overlap_1][:end])
       puts "Overlap 2:"
       puts "#{formated_time(@data[:overlap_2][:start])} - #{formated_time(@data[:overlap_2][:end])}"
-
-      show_it(@data[:overlap_2][:start], @data[:overlap_2][:end])
-
+      timeline(@data[:overlap_2][:start], @data[:overlap_2][:end])
       @data
     end
 
@@ -89,6 +43,53 @@ module TimeOverlap
 
     def formated_time(time)
       time.strftime("%T %:z")
+    end
+
+    def timeline(start_time, end_time)
+      print AM
+
+      (0..23).each do |hour|
+        print NOON if hour == 12
+        print SIX_AM if hour == 6
+        print SIX_PM if hour == 18
+
+        if start_time.hour < end_time.hour
+          if (start_time.hour..end_time.hour).cover?(hour)
+            if end_time.hour != hour
+              print AVAILABLE_SLOT
+            else
+              print EMPTY_SLOT
+            end
+          else
+            print EMPTY_SLOT
+          end
+        else
+          if start_time.hour <= 12
+            if (end_time.hour..start_time.hour).cover?(hour)
+              if end_time.hour != hour
+                print AVAILABLE_SLOT
+              else
+                print EMPTY_SLOT
+              end
+            else
+              print EMPTY_SLOT
+            end
+          else
+            if (end_time.hour..start_time.hour).cover?(hour)
+              print EMPTY_SLOT
+            else
+              if end_time.hour != hour
+                print AVAILABLE_SLOT
+              else
+                print EMPTY_SLOT
+              end
+            end
+          end
+        end
+      end
+      print PM
+      puts ""
+      separator
     end
   end
 end
